73ab99942ea74c4ab60d059d8d76eaf05e5174db
Marlowe <marlowescotts@gmail.com>
2025-12-17 07:08:25 -0600

    added reuters

--- lib/rss-feeds.ts
+++ lib/rss-feeds.ts
@@ -15,6 +15,7 @@ export const RSS_FEEDS = {
     'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',
     'https://www.theguardian.com/world/rss',
     'https://feeds.npr.org/1004/rss.xml',
+    'https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3Aen',
   ],
   'world-politics': [
     'https://rssmix.com/u/8354820/rss.xml',
@@ -24,6 +25,7 @@ export const RSS_FEEDS = {
     'http://rss.cnn.com/rss/cnn_allpolitics.rss',
     'http://feeds.foxnews.com/foxnews/politics',
     'https://www.politico.com/rss/politics08.xml',
+    'https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3Aen',
   ],
   'positive-news': [
     'https://www.goodnewsnetwork.org/feed/',
@@ -41,12 +43,14 @@ export const RSS_FEEDS = {
     'https://www.newsmax.com/rss/Politics/',
     'https://www.salon.com/category/politics/feed/',
     'https://www.dailykos.com/blogs/main.rss',
+    'https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3Aen',
   ],
   usa: [
     'https://rssmix.com/u/8354824/rss.xml',
     'https://feeds.npr.org/1003/rss.xml',
     'https://rss.nytimes.com/services/xml/rss/nyt/US.xml',
     'http://rssfeeds.usatoday.com/UsatodaycomNation-TopStories',
+    'https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3Aen',
   ],
   minnesota: [
     'https://www.startribune.com/local/index.rss2',
